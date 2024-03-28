adcdb2fd06c0fb33c8d445092be88bc2c50e3953
ernest <shiwb@shideMac-mini.local>
2024-03-28 18:07:29 +0800

    hunter: change osmosis to injectiove

--- chainData_bak/relayer/relayer-run.sh
+++ chainData_bak/relayer/relayer-run.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 rly config init --home "./relayer"
-rly chains add evmos osmosis --home "./relayer"
+rly chains add evmos injective --home "./relayer"
 
 rly keys restore evmos evmos1 \
 "test test test test test test test test test test test junk" \
 --home "./relayer"
 
-rly keys restore osmosis osmosis1 \
+rly keys restore injective injective1 \
 "test test test test test test test test test test test junk" \
 --home "./relayer"
 
 rly keys use evmos evmos1 --home "./relayer"
-rly keys use osmosis osmosis1 --home "./relayer"
+rly keys use injective injective1 --home "./relayer"
 
 rly q balance evmos evmos1 --home "./relayer"
-rly q balance osmosis osmosis1 --home "./relayer"
+rly q balance injective injective1 --home "./relayer"
 
 
 rly paths fetch --home "./relayer"
