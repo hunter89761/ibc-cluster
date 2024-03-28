450a72eed2593b1e98f25b7249ab4c8f640cc41e
ernest <shiwb@shideMac-mini.local>
2024-03-28 17:59:29 +0800

    hunter: change relayer-run.sh

--- chainData_bak/relayer/relayer-run.sh
+++ chainData_bak/relayer/relayer-run.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-sleep 100
+rly config init --home "./relayer"
+rly chains add cosmoshub osmosis --home "./relayer"
+
+rly keys restore cosmoshub cosmoshub1 \
+"test test test test test test test test test test test junk" \
+--home "./relayer"
+
+rly keys restore osmosis osmosis1 \
+"test test test test test test test test test test test junk" \
+--home "./relayer"
+
+rly keys use cosmoshub cosmoshub1 --home "./relayer"
+rly keys use osmosis osmosis1 --home "./relayer"
+
+rly q balance cosmoshub cosmoshub1 --home "./relayer"
+rly q balance osmosis osmosis1 --home "./relayer"
+
+
+rly paths fetch --home "./relayer"
+
+rly paths list --home "./relayer"
+rly start --home "./relayer"
