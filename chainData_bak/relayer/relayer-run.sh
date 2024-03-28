ccaf58176fd5768bd171c3fee1c759d78c39f278
ernest <shiwb@shideMac-mini.local>
2024-03-28 18:18:02 +0800

    hunter: support relayer

--- chainData_bak/relayer/relayer-run.sh
+++ chainData_bak/relayer/relayer-run.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 rly config init --home "./relayer"
-rly chains add evmos injective --home "./relayer"
+rly chains add evmos kujira --home "./relayer"
 
 rly keys restore evmos evmos1 \
 "test test test test test test test test test test test junk" \
 --home "./relayer"
 
-rly keys restore injective injective1 \
+rly keys restore kujira kujira1 \
 "test test test test test test test test test test test junk" \
 --home "./relayer"
 
 rly keys use evmos evmos1 --home "./relayer"
-rly keys use injective injective1 --home "./relayer"
+rly keys use kujira kujira1 --home "./relayer"
 
 rly q balance evmos evmos1 --home "./relayer"
-rly q balance injective injective1 --home "./relayer"
+rly q balance kujira kujira1 --home "./relayer"
 
 
 rly paths fetch --home "./relayer"
