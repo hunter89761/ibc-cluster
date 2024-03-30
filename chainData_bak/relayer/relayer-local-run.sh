bb6e3fbba256ff464339fc21fb45bae4af447747
ernest <shiwb@shideMacBook-Pro.local>
2024-03-30 14:55:27 +0800

    hunter: add accounts

--- chainData_bak/relayer/relayer-local-run.sh
+++ chainData_bak/relayer/relayer-local-run.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 
+rly config init
+rly chains add --file ./relayer/evmos-chain.json evmos
+echo "rly chains added evmos"
 
-sleep 1000
+rly chains add --file ./relayer/celestia-chain.json celestia
+echo "rly chains added celestia"
 
-rly chains add --url https://raw.githubusercontent.com/cosmos/relayer/main/docs/example-configs/cosmoshub-4.json cosmoshub
+rly chains show evmos
+rly chains show celestia
+
+echo "begin sleeping 10 seconds"
+sleep 10
+
+rly keys restore evmos evmos1 \
+"smart usage nerve wedding derive math inmate convince pond hidden resemble wagon language wheat cigar various sniff crumble tired evil much buyer decorate travel"
+
+rly keys restore celestia celestia1 \
+"smart usage nerve wedding derive math inmate convince pond hidden resemble wagon language wheat cigar various sniff crumble tired evil much buyer decorate travel"
+
+
+rly keys use evmos evmos1
+rly keys use celestia celestia1
+
+
+rly q balance evmos evmos1
+rly q balance celestia celestia1
