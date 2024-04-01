d42da53a43b09654bb1bdc0b4af19b8ccade3d8b
ernest <shiwb@shideMac-mini.local>
2024-04-01 17:45:31 +0800

    hunter: add rly paths add

--- chainData_bak/relayer/relayer-local-run.sh
+++ chainData_bak/relayer/relayer-local-run.sh
@@ -13,16 +13,27 @@ rly chains show celestia
 echo "begin sleeping 10 seconds"
 sleep 10
 
-rly keys restore evmos evmos1 \
-"smart usage nerve wedding derive math inmate convince pond hidden resemble wagon language wheat cigar various sniff crumble tired evil much buyer decorate travel"
+#
+#rly keys add evmos validator1
+#rly keys add celestia node0
 
-rly keys restore celestia celestia1 \
-"smart usage nerve wedding derive math inmate convince pond hidden resemble wagon language wheat cigar various sniff crumble tired evil much buyer decorate travel"
+mv ./relayer/keys ~/.relayer/
+
+sleep 10
+
+rly keys use evmos validator1
+rly keys use celestia node0
 
 
-rly keys use evmos evmos1
-rly keys use celestia celestia1
+rly q balance evmos validator1
+rly q balance celestia node0
 
 
-rly q balance evmos evmos1
-rly q balance celestia celestia1
+# rly paths new [src-chain-id] [dst-chain-id] [path-name] [flags]
+rly paths new evmos_9000-1 celestia-9002 test_path
+
+echo "rly trnasact link test_path"
+
+rly transact link test_path
+
+sleep 1000
