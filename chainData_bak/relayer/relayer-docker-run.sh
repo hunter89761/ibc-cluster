e766518e8a1cff9442359997242635c1e5dc5389
ernest <shiwb@shideMac-mini.local>
2024-04-02 20:12:53 +0800

    hunter: support docker-relayer

--- chainData_bak/relayer/relayer-docker-run.sh
+++ chainData_bak/relayer/relayer-docker-run.sh
@@ -1,35 +1,33 @@
 #!/bin/bash
 
 rly config init
-rly chains add --file ./relayer/evmos-chain.json evmos
-echo "rly chains added evmos"
+rly chains add --file ./relayer/cele-chain.json cele
+echo "rly chains added cele"
 
 rly chains add --file ./relayer/celestia-chain.json celestia
 echo "rly chains added celestia"
 
-rly chains show evmos
+rly chains show cele
 rly chains show celestia
 
 echo "begin sleeping 10 seconds"
 sleep 10
 
 #
-#rly keys add evmos validator1
+#rly keys add cele validator1
 #rly keys add celestia node0
 
 mv ./relayer/keys ~/.relayer/
 
-sleep 10
-
-rly keys use evmos validator1
+rly keys use cele validator1
 rly keys use celestia node0
 
 
-rly q balance evmos validator1
+rly q balance cele validator1
 rly q balance celestia node0
 
 # rly paths new [src-chain-id] [dst-chain-id] [path-name] [flags]
-rly paths new evmos_9000-1 celestia-9002 test_path
+rly paths new cele-9000 celestia-9002 test_path
 
 echo "rly trnasact link test_path"
 
