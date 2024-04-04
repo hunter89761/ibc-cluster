62821ed1bf28054a2cf764c7fff016d953f6926a
ernest <shiwb@shideMac-mini.local>
2024-04-04 18:31:56 +0800

    hunter: add relayer

--- localnet/run-local.sh
+++ localnet/run-local.sh
@@ -29,15 +29,18 @@ rly q balance celestia node0
 rly paths new cele-9000 celestia-9002 test_path
 
 echo "rly transact clients test_path"
-rly transact clients test_path
+rly transact link test_path --debug
 
-echo "rly transact connection test_path"
-rly transact connection test_path --block-history 40 --debug
-
-
-echo "rly transact channel test_path"
-sleep 10
-rly transact channel test_path --src-port transfer --dst-port transfer --order unordered --version ics20-1
+#echo "rly transact connection test_path"
+#rly transact connection test_path --block-history 40 --debug
+#
+#
+#echo "rly transact channel test_path"
+#sleep 10
+#rly transact channel test_path --src-port transfer --dst-port transfer --order unordered --version ics20-1
 
 
 #rly transact link test_path --version "{\"fee_version\":\"ics29-1\",\"app_version\":\"ics20-1\"}"
+
+# echo "rly start"
+rly start --debug
