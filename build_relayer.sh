24524d40a2c942fe597dcbfd6c123b721f88cf22
ernest <shiwb@shideMac-mini.local>
2024-04-04 23:20:13 +0800

    hunter: add build webbshi/celestia-app-ucele

--- build_relayer.sh
+++ build_relayer.sh
@@ -17,3 +17,13 @@ docker push webbshi/ibc-relayer
 #git checkout v2.4.0
 #make install
 # the binary file is rly
+
+
+git clone git@github.com:hunter89761/celestia-app.git
+cd celestia-app
+
+git checkout ernest/da-chain-ucele
+docker build . -t webbshi/celestia-app-ucele:v1.6.0
+
+docker push webbshi/celestia-app-ucele:v1.6.0
+cd ..
